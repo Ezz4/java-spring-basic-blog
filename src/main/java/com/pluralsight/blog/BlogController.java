f87ee5c093d37edd41119f6478a0d867c245d784
Ezz4 <zuryta92@gmail.com>
2021-04-07 21:29:26 -0500

    fix according test

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,20 +1,16 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Controller
 public class BlogController {
-
     @RequestMapping("/")
-    public String listPosts(ModelMap map){
-        map.put("title", "Blog Post1");
+    public String listPosts(ModelMap modelMap){
+        modelMap.put("title", "Blog Post 1");
         return "home";
     }
 
