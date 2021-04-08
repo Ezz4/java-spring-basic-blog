224c56126f559886d053dc9524ffc019bed1d45a
Ezz4 <zuryta92@gmail.com>
2021-04-07 21:18:34 -0500

    Create a request mapping method and display data in the template

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,23 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap map){
+        map.put("title", "Blog Post1");
+        return "home";
+    }
+
+
 
 }
